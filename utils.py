3adab866ae8e9f7af06fa21a645e388015858f7c
yohanstra <jreinstra@gmail.com>
2016-10-08 17:29:11 -0400

    syntax fix

--- utils.py
+++ utils.py
@@ -149,4 +149,4 @@ def distance(lat1, lon1, lat2, lon2, unit):
 
 if __name__ == "__main__":
     print "loading sketch factor..."
-    print "sketchiness:", calc_sketchiness(42.3418956, -83.0602594, 42.3018956, -83.0202594)
+    print "sketchiness:", calc_sketchiness(42.3418956, -83.0602594, 42.3018956, -83.0202594, {})
