95c200a9648b112ecef189c46f35687f145dae58
jasonscharff <jasonscharff@gmail.com>
2016-10-09 09:03:56 -0400

    Be less stupid.

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def wit_process_message(recipient_id, message):
                         separatorIndex = key_of_first.index('_')
                         transitMode = key_of_first[:separatorIndex]
                         indexInGmaps = key_of_first[separatorIndex+1:]
-                        summary = gmaps[transitMode]['summary'][indexInGmaps]['summary']
+                        summary = gmaps[transitMode]['routes'][indexInGmaps]['summary']
 
                         fb_send_reply(recipient_id, str(ranked))
                         # Tell them to wait, then do the magic
